4fffcaa1f200186b6c480362619504a9b0e6a51b
ypatel2800 <ypatel280@tcolc.aspirelp.uk>
2025-01-10 00:43:38 +0000

    update

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const today = new Date();
-const exam = new Date("2025-05-05");
-const timeDifference = exam - today;
-
-const daysUntilExam = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
-const weeksUntilExam = Math.floor(daysUntilExam / 7);
-const remainingDays = daysUntilExam % 7;
-
-document.getElementById("date").textContent = `🚨Days until GCSEs: ${daysUntilExam}🚨`;
-=======
 const today = new Date();
 const exam = new Date("2025-05-05");
 const timeDifference = exam - today;
@@ -18,4 +7,3 @@ const weeksUntilExam = Math.floor(daysUntilExam / 7);
 const remainingDays = daysUntilExam % 7;
 
 document.getElementById("date").textContent = `🚨Days until GCSE: ${daysUntilExam}🚨`;
->>>>>>> dcb852c (added subjects)
